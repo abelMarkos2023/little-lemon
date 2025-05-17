e9a2c13358abebfe309926a950a904e5cfd0cb82
abelMarkos2023 <abelmarkos2023@gmail.com>
2025-05-17 17:23:00 +0300

    adding open graph meta

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+        <header>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p> We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+        </header>
+)
+}
+
+export default Header
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p> We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+    </main>
+  )
+}
+
+export default Main
