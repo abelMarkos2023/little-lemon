c3bfdbcae8ffd410035a912c070607f5185ed6a5
abelMarkos2023 <abelmarkos2023@gmail.com>
2025-06-07 13:08:32 +0300

    fialaizing the project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Navbar/Navbar';
 import Specials from './components/Specials/Specials';
 import HeroSection from './components/Hero/Hero';
+import Form from './components/Form/Form';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <HeroSection />
       <Specials />
+      <Form />
 
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
+
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="logo">🍋 Little Lemon</div>
-      <nav>
+
+      <button
+        className="menu-toggle"
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label="Toggle menu"
+      >
+        ☰
+      </button>
+
+      <nav className={isOpen ? "nav open" : "nav"}>
         <ul>
           <li>Home</li>
           <li>About</li>
